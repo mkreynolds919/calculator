9afcff519bc1cefe146ae6f07b1a1b0031b13cb4
mkreynolds919 <mkreynolds919@gmail.com>
2024-04-24 22:15:10 -0500

    Create operate function
    Assign displayContent variables

--- myscripts.js
+++ myscripts.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function operate(a, b, operator) {
+    switch(operator) {
+        case "+": 
+            add(a,b);
+        case "-":
+            subtract(a,b);
+        case "x":
+            multiply(a,b);
+        case "/":
+            divide(a,b);
+    }
+}
+
+const display = document.querySelector(".display-container");
+let displayContent = display.textContent;
+const arr = displayContent.split(' ');
+let a = arr[0], operator = arr[1], b = arr[2];
+
