32fe6d7aad621e1c27860dbab4c3b40f41e1f8e2
mkreynolds919 <mkreynolds919@gmail.com>
2024-04-25 19:05:47 -0500

    Restore all files

---
+++ myscripts.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, operator) {
+    switch(operator) {
+        case "+": 
+            add(a,b);
+        case "-":
+            subtract(a,b);
+        case "x":
+            multiply(a,b);
+        case "/":
+            divide(a,b);
+    }
+}
+
+const display = document.querySelector(".display-container");
+let displayContent = display.textContent;
+const arr = displayContent.split(' ');
+let a = arr[0], operator = arr[1], b = arr[2];
+
